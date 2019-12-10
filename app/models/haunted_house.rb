a389a59f0e88504f74253538cc70c8c4b8e6c48f
clean-command-1851 <ed7901979t@gmail.com>
2019-12-10 11:39:51 +0000

    Done.

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,15 @@
-# Create your HauntedHouse class here
+class Haunted_houses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location
+      t.string :theme
+      t.string :price
+      t.string :family_friendly
+      t.string :opening_time
+      t.string :closing_time
+      t.string :long_description
+    end
+  end
+end
+end
